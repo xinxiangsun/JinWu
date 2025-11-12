48ba82b67b0c34726154df38d2615e7ec88a352b
Xinxiang Sun <sunxx@nao.cas.cn>
2025-11-12 22:06:33 +0800

    修改：     src/jinwu/_version.py

--- src/jinwu/_version.py
+++ src/jinwu/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.0.9.dev0'
-__version_tuple__ = version_tuple = (0, 0, 9, 'dev0')
+__version__ = version = '0.0.7.dev0'
+__version_tuple__ = version_tuple = (0, 0, 7, 'dev0')
 
-__commit_id__ = commit_id = 'g391846c74'
+__commit_id__ = commit_id = 'ga1edf71ce'
